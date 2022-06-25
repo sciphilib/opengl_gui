7c4dc10373dac4e7492436bcca3595d280d3d12b
sciphilib <sciphilib@gmail.com>
2022-06-25 12:25:59 +0600

    Done: 1) Camera class (lookAt with forward/backward/left/right movement, yaw/pitch rotations and zoom). 2) Textures.

--- main.cpp
+++ main.cpp
@@ -138,6 +138,7 @@ int main() {
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
 	float vertices[] = {
+						  //textures coords
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
 	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
